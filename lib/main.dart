6b13751428576b4e2dcfda95be6913eef25df431
jaivsh <aarshorai22@gmail.com>
2024-02-10 19:21:19 +0530

    login page build stage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //final gsheets = GSheets(credentials);
+  //final ss =  await gsheets.spreadsheet(spreadsheetid);
+  //var sheet = ss.workSheetByTitle('Sheet1');
+  runApp(MaterialApp(debugShowCheckedModeBanner: false,home:MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -14,6 +18,13 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(title: Image.asset('assets/images/branding_top.png'), backgroundColor: Color(0xff1B374C),),
+          body: Container(color: Color(0xfffeebdc),
+            child: Center(child: Text('Loginpage')
+          ),)));
   }
+
 }
